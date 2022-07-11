b710e0d1b0d95fb7d99707575e3e31b7f46d5555
khanshorif331 <khanshorif331@gmail.com>
2022-07-11 21:01:05 +0600

    dark mode done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Box, Stack } from '@mui/material'
+import { Box, createTheme, Stack, ThemeProvider } from '@mui/material'
+import { useState } from 'react'
 import Add from './components/Add'
 import Feed from './components/Feed'
 import Navbar from './components/Navbar'
@@ -6,16 +7,24 @@ import Rightbar from './components/Rightbar'
 import Sidebar from './components/Sidebar'
 
 function App() {
+	const [mode, setMode] = useState('light')
+	const darkTheme = createTheme({
+		palette: {
+			mode: mode,
+		},
+	})
 	return (
-		<Box>
-			<Navbar></Navbar>
-			<Stack direction='row' spacing={2} justifyContent='space-between'>
-				<Sidebar></Sidebar>
-				<Feed></Feed>
-				<Rightbar></Rightbar>
-			</Stack>
-			<Add></Add>
-		</Box>
+		<ThemeProvider theme={darkTheme}>
+			<Box bgcolor={'background.default'} color={'text.primary'}>
+				<Navbar></Navbar>
+				<Stack direction='row' spacing={2} justifyContent='space-between'>
+					<Sidebar setMode={setMode} mode={mode}></Sidebar>
+					<Feed></Feed>
+					<Rightbar></Rightbar>
+				</Stack>
+				<Add></Add>
+			</Box>
+		</ThemeProvider>
 	)
 }
 
--- src/components/Add.js
+++ src/components/Add.js
@@ -59,7 +59,8 @@ const Add = () => {
 				<Box
 					width={400}
 					height={280}
-					bgcolor='white'
+					bgcolor={'background.default'}
+					color={'text.primary'}
 					p={3}
 					borderRadius={5}
 				>
